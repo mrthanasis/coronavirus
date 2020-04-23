6d32059ec85cbc70bfd7c225acd04986613d08b1
Thanasis Chrysovergis <misterthanasis@gmail.com>
2020-04-23 19:06:53 +0300

    Update util.js

--- src/lib/util.js
+++ src/lib/util.js
@@ -33,4 +33,21 @@ export function commafy(value) {
   numberString = numberString.join('');
 
   return numberString;
+  }
+
+
+/**
+ * friendlyDate
+ * @description Takes in a date value and returns a friendly version
+ */
+
+export function friendlyDate(value) {
+  const date = new Date(value);
+  return new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: 'numeric'
+  }).format(date);
 } 
