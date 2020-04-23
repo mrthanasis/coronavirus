2a67296474377af6302b394ac8a3b37af026624e
Thanasis Chrysovergis <misterthanasis@gmail.com>
2020-04-23 18:17:09 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -108,12 +108,7 @@ const IndexPage = () => {
       </Helmet>
       <Map {...mapSettings} />
       <Container type="content" className="text-center home-start">
-        <h2>Still Getting Started?</h2>
-        <p>Run the following in your terminal!</p>
-        <pre>
-          <code>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</code>
-        </pre>
-        <p className="note">Note: Gatsby CLI required globally for the above command</p>
+       
       </Container>
     </Layout>
   );
