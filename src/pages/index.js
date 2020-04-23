540ec511e3b58f72358506e9e4b690f59005696e
Thanasis Chrysovergis <misterthanasis@gmail.com>
2020-04-23 18:15:21 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -97,7 +97,7 @@ const IndexPage = () => {
   }
   const mapSettings = {
     center: CENTER,
-    defaultBaseMap: 'OpenStreetMap',
+    defaultBaseMap: 'Mapbox',
     zoom: DEFAULT_ZOOM,
     mapEffect
   };
